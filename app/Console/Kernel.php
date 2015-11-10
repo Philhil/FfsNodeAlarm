5075b8d275ac6832d354ef15d7cd970bddfc80b5
Phil <kaeufer@mediativ.de>
2015-11-10 11:08:18 +0100

    remove test command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\Inspire::class,
         \App\Console\Commands\getNodeJson::class,
         \App\Console\Commands\checkTasks::class,
     ];
@@ -26,8 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')->hourly();
-
         $schedule->command('getNodeJson')->everyFiveMinutes()->withoutOverlapping();
 
         $schedule->command('checkTasks')->everyMinute()->withoutOverlapping();
