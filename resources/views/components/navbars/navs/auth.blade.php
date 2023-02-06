0316cfe77427795fecc6049927aa818688af7cac
Flip <philippe@pilarkto.net>
2023-02-06 23:16:11 +0100

    improve logout icon

--- resources/views/components/navbars/navs/auth.blade.php
+++ resources/views/components/navbars/navs/auth.blade.php
@@ -19,7 +19,7 @@
             <ul class="navbar-nav  justify-content-end">
                 <li class="nav-item d-flex align-items-center">
                     <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-                        <i onclick="event.preventDefault();document.getElementById('logout-form').submit();" class="fa fa-user me-sm-1"></i>
+                        <i onclick="event.preventDefault();document.getElementById('logout-form').submit();" class="fa fa-sign-out me-sm-1"></i>
                         <span class="d-sm-inline d-none"
                             onclick="event.preventDefault();document.getElementById('logout-form').submit();">Sign
                             Out</span>
